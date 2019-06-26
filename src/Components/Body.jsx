1568e5400485e3c12ae3f85ff761ecf8dbdbd778
Kye <kye@BGGLOBAL9334.local>
2019-06-26 15:14:24 +0100

    warning removals.

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import Videoplayer from './Videoplayer'
 
 const Container = styled.div`
   margin: 0;
--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import Fade from 'react-reveal/Fade';
-import ImageMapper from './ImageMapper'
 import VideoPlayer from './VideoPlayer'
 
 import image from '../images/casino-icon_bold.png';
