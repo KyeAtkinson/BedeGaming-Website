7e4d381e302ab07b4d70c57664ce0275073e2065
Kye <kye@BGGLOBAL9334.local>
2019-10-04 16:58:16 +0100

    fix 2...

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 // import Navigation from './components/Navigation';
-import Body from './components/Body';
+// import Body from './components/Body';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       {/* <Navigation /> */}
-      <Body />
+      {/* <Body /> */}
+      <h1>Hello michael. and dog.</h1>
     </div>
   );
 }
