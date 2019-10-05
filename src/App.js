ad0769b48fcab9804caa6accf74d48aee9cc48bd
Kye <kye@BGGLOBAL9334.local>
2019-10-05 16:38:21 +0100

    woops this.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-// import Navigation from './components/Navigation';
-// import Body from './components/Body';
+import Navigation from './components/Navigation';
+import Body from './components/Body';
 import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navigation /> */}
-      {/* <Body /> */}
+      <Navigation />
+      <Body />
       <h1>Hello michael. and dog.</h1>
       <Home />
     </div>
